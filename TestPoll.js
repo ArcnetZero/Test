f79c3d2166eb89e05ac787cd276022284fdd9e19
Brenden Carney <bcarney@student.icablazers.org>
2015-02-20 09:30:12 -0600

    test 1

---
+++ TestPoll.js
@@ -0,0 +1,21 @@
+var main=function(answer) {
+    if (answer = 1) {
+        console.log("1s")
+    }
+    else if (answer = 2) {
+        console.log("2s")
+    }
+    else if (answer = 3) {
+        console.log("3s")
+    }
+    else {
+        console.log("FAIL")
+    }
+}
+$(document).ready(main);
+
+
+
+/**
+ * Created by admin on 2/13/15.
+ */
